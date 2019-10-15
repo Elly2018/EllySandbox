871e110d55580f3da3bba4316824c8435dedbaf1
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2019-10-15 08:29:09 +0800

    Change To c# framework

---
+++ EllySandbox/EllySandbox/EllySandbox.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.ES30;
+
+namespace EllySandbox
+{
+    class EllySandbox : GameWindow
+    {
+        public EllySandbox(int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
+        {
+
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            base.OnUnload(e);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
