236e55abefa5bc756cacc885513d8f5d8ea8db93
凱爾 朱 <chu.elly@hotmail.com.tw>
2019-10-21 14:44:49 +0800

    Update debug system

--- EllySandbox/EllySandbox/Engine/Module/Debug.cs
+++ EllySandbox/EllySandbox/Engine/Module/Debug.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using EllySandbox.Engine.Base;
 using EllySandbox.Engine.Struct;
-using System.Collections.Generic;
 using EllySandbox.Engine.Helper;
-using System.Text;
 
 namespace EllySandbox.Engine.Module
 {
+    /// <summary>
+    /// 
+    /// There are two file output:
+    /// -Lastest: file is single forward output, and when controller clean the log or delete line, it will effect on the file.
+    /// -Datetime: file is store all the information and log histroy. and refresh everytime it update.
+    /// 
+    /// </summary>
     class Debug : ModuleBase
     {
         public enum DebugType
@@ -54,21 +61,28 @@ namespace EllySandbox.Engine.Module
             List<string> s = new List<string>(Message);
             s.Add("[" + tag + "] " + o.ToString());
             Message = s.ToArray();
-            FileUpdate();
+            FileUpdate("[" + tag + "] " + o.ToString());
         }
         /// <summary>
         /// writing message on end of target logging text file
         /// </summary>
         /// <param name="o"></param>
-        private static void FileUpdate()
+        private static void FileUpdate(string newLine)
         {
             EPath epath = HelperFactory.GetModuleByType<EPath>();
 
+            /* Lastest */
+            string[] currentLastest = File.ReadAllLines(Path.Combine(epath._ApplicationConfigPath(), EPath.ApplicationData, EPath.ApplicationLog, "Lastest log.txt"));
+            List<string> _currentLastest = new List<string>(currentLastest);
+            _currentLastest.Add(newLine);
+            File.WriteAllLines(Path.Combine(epath._ApplicationConfigPath(), EPath.ApplicationData, EPath.ApplicationLog, "Lastest log.txt"), _currentLastest.ToArray());
+
+            /* Datetime */
             if (File.Exists(Path.Combine(epath._ApplicationConfigPath(), EPath.ApplicationData, EPath.ApplicationLog, Filename)))
                 File.Delete(Path.Combine(epath._ApplicationConfigPath(), EPath.ApplicationData, EPath.ApplicationLog, Filename));
-
             File.WriteAllLines(Path.Combine(epath._ApplicationConfigPath(), EPath.ApplicationData, EPath.ApplicationLog, Filename), Message);
-            File.WriteAllLines(Path.Combine(epath._ApplicationConfigPath(), EPath.ApplicationData, EPath.ApplicationLog, "Lastest log.txt"), Message);
+
+
         }
     }
 }
